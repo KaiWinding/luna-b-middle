1472c1a797dd81d42b67e2b7d470a28328a3d4c6
KaiWinding <879253745@qq.com>
2023-09-22 19:14:24 +0800

    refactor

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,18 @@ const { imageService } = require("../services/index");
 const imageController = {
   async generateImage(ctx) {
     const description = ctx.request.body.description;
-    console.log('hello world');
-    console.log('description = ', description);
-    const image = await imageService.generateImage(description);
-    ctx.body = { image };
+    console.log("hello world");
+    console.log("description = ", description);
+    const res = await imageService.generateImage(description);
+    ctx.body = { id: res };
+  },
+  async pollingImage(ctx) {
+    const id = ctx.request.body.id;
+    console.log("hello world");
+    console.log("description = ", description);
+    const res = await imageService.pollingImage(id);
+    console.log('res = ', res);
+    ctx.body = res;
   },
 };
 
--- services/index.js
+++ services/index.js
@@ -1,8 +1,31 @@
 const { Midjourney } = require("midjourney");
 const { account } = require("../utils");
 
+const mjRequestMap = {};
+
+const getImageServiceId = () => {
+  let imageServiceId;
+  do {
+    const timestamp = date.getTime();
+    // 生成随机数
+    const randomNum = Math.floor(Math.random() * 100000)
+      .toString()
+      .padStart(5, "0");
+    // 组合订单编号
+    imageServiceId = `${timestamp}-${randomNum}`;
+  } while (!mjRequestMap[imageServiceId]);
+
+  return imageServiceId;
+};
+
 const imageService = {
   async generateImage(description) {
+    const imageServiceId = getImageServiceId();
+    mjRequestMap[imageServiceId] = {
+      status: 0,
+      id: imageServiceId,
+      description,
+    };
     const client = new Midjourney({
       ServerId: account.SERVER_ID,
       ChannelId: account.CHANNEL_ID,
@@ -19,17 +42,25 @@ const imageService = {
         "asian princess in wes anderson style, dressed in local uniform in Azure Lagoon, epic shot , --ar 16:9 --no glasses --stylize 400 --q .5 --v 5.2",
         (uri) => {
           console.log("loading123---", uri);
+
+          mjRequestMap[imageServiceId].url = url;
         }
       )
       .then(function (msg) {
         console.log("msg123", msg);
+        mjRequestMap[imageServiceId].url = msg.uri;
+        mjRequestMap[imageServiceId].status = 1;
       });
     // 实现生成图片的业务逻辑
     // 返回生成的图片 URL 或数据
-    return "image_url_or_data";
+    return imageServiceId;
+  },
+  async pollingImage(imageServiceId) {
+    return mjRequestMap[imageServiceId];
   },
 };
 
 module.exports = {
   imageService,
+  pollingImage,
 };
