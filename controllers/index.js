897cd4c8bdee036665570293be04df43fedeafcb
KaiWinding <879253745@qq.com>
2023-09-23 09:35:10 +0800

    refactor

--- controllers/index.js
+++ controllers/index.js
@@ -14,9 +14,9 @@ const imageController = {
     };
   },
   async pollingImage(ctx) {
-    const id = ctx.request.body.id;
+    const id = ctx.request.body.taskId;
     console.log("hello world");
-    console.log("description = ", description);
+    console.log("description = ", id);
     const taskResult = await imageService.pollingImage(id);
     console.log('res = ', res);
     ctx.body = {
