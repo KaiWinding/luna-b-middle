794e6044190559bc381506f6673f12a566a297a1
KaiWinding <879253745@qq.com>
2023-09-24 17:12:14 +0800

    refactor

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,22 @@ const { imageService } = require("../services/index");
 const imageController = {
   async generateImage(ctx) {
     const description = ctx.request.body.description;
+    const Jwtheader = ctx.request.headers["Jwtheader"];
+    if (!Jwtheader) {
+      ctx.status = 4002;
+      ctx.body = {
+        code: -1,
+      };
+      return;
+    }
     console.log("hello world");
     console.log("description = ", description);
     const res = await imageService.generateImage(description);
     ctx.body = {
-        code: 1,
-        data: {
-            taskId: res
-        }
+      code: 1,
+      data: {
+        taskId: res,
+      },
     };
   },
   async pollingImage(ctx) {
@@ -19,8 +27,8 @@ const imageController = {
     console.log("description = ", id);
     const taskResult = await imageService.pollingImage(id);
     ctx.body = {
-        code: 1,
-        data: taskResult
+      code: 1,
+      data: taskResult,
     };
   },
 };
